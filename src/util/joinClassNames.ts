b7118f60e7e235d6d31dbce06ec5a69f79993d1f
Romanhudaj11 <romanhudaj1@gmail.com>
2025-01-22 20:16:35 -0500

    Updates

--- src/util/joinClassNames.ts
+++
@@ -1,6 +0,0 @@
-export const joinClassNames = (className: any, ...classNames: any) => {
-    // handle the case where some might be undefined
-    const classes = [className]
-    classes.push(...classNames)
-    return classes.filter((c) => String(c)).join(" ");
-}
