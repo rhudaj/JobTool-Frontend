7b6b583432766c9c7994c1d4c7eaa4bc9c243966
Romanhudaj11 <romanhudaj1@gmail.com>
2025-03-04 17:58:11 -0500

    combine app & index files

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,49 @@
+import "./index.sass";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import ResumeBuilder from "./sections/ResumeBuilder/ResumeBuilder";
+import CLBuilder from "./sections/CLBuilder/CoverLetterBuilder";
+import JobAnalyze from "./sections/JobAnalyze/JobAnalyze";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
+const tabs = [
+    {
+        name: "Resume Builder",
+        content: <ResumeBuilder/>,
+    },
+    {
+        name: "Letter",
+        content: <CLBuilder/>,
+    },
+    {
+        name: "Job Analyze",
+        content: <JobAnalyze/>
+    },
+];
 
-root.render(
+function App() {
+    return (
+        <div className="app-root-container">
+            <div id="app-header">
+                <h1>Job Tool</h1>
+            </div>
+            <TabGroup id="app-content">
+                <TabList id="side-menu" style={{width: "min-content"}}>
+                    {tabs.map(tab =>
+                        <Tab className="menu-item" as="div" key={tab.name}>{tab.name}</Tab>
+                    )}
+                </TabList>
+                <TabPanels id="section-container">
+                    {tabs.map(tab =>
+                        <TabPanel>{tab.content}</TabPanel>
+                    )}
+                </TabPanels>
+            </TabGroup>
+        </div>
+    );
+}
+
+ReactDOM.createRoot(
+    document.getElementById("root")
+).render(
     <App />
 );
